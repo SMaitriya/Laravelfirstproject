957aacbbc3289ebc58fb86a9adf76bf50f1bf5ee
Maitriya <maitriya@outlook.fr>
2023-12-16 19:39:32 +0100

    add about page and update links

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,11 +4,13 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
     
-        <title>Hello City</title>
+        <title>About Us | Hello City</title>
     </head>
     <body>
-        <h1> Hello from paris </h1>
-        <p>It's currently {{ date('H:i A') }}</p>
+       
+        <p>Built with &hearts; by Maitriya</p>
+
+        <p><a href="/"> Revenir à la page d'accueil</a></p>
     <footer>
         <p>&copy; Copyright {{date('Y')}} &middot; <a href="/about-us">About us</a></p>
        
