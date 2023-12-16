1225a4dc76a96220369cf6dde037a7cd1d46c772
Maitriya <maitriya@outlook.fr>
2023-12-16 20:04:27 +0100

    Remove duplication by using app layout

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,18 +1,11 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-    
-        <title>About Us | Hello City</title>
-    </head>
-    <body>
+
+@extends('base')
+
+@section('content')
        
         <p>Built with &hearts; by Maitriya</p>
 
         <p><a href="/"> Revenir à la page d'accueil</a></p>
-    <footer>
-        <p>&copy; Copyright {{date('Y')}}</p>
-       
-    </body>
-</html>
+
+     
+@endsection
