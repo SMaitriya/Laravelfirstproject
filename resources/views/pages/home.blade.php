50b6883142560be2860e990af378561162dfedeb
Maitriya <maitriya@outlook.fr>
2023-12-17 23:27:59 +0100

    update title to Maitriya laravel

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -5,10 +5,10 @@
 
 @section('content')
 
-      <img src={{ asset('images/M.png')}} width=500px height=300px alt="ordi" class="mt-12 rounded shadow-md">
+      <img src={{ asset('images/M.png')}} width=500px height=300px alt="ordi" class="mt-12 rounded shadow-md ">
 
         <h1 class="mt-5 text-3xl sm:text-5xl font-semibold text-indigo-600 "> Laravel Project </h1>
-        <p class="text-zinc-700 mt-4" >It's currently {{ date('H:i A') }}</p>
+        <p class="text-zinc-700 mt-4 hover:scale-150" >It's currently {{ date('H:i A') }}</p>
 
 @endsection
 
